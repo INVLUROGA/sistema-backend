1788a120d14dd84c8f98c55536807b00a5ff8382
personalTrainingp <crosales@personaltraining.com.pe>
2025-02-11 00:24:58 -0500

    retrocediendo

--- routes/ZkTeco/zk.router.js
+++ routes/ZkTeco/zk.router.js
@@ -1,6 +1,15 @@
 const { Router } = require("express");
 const { obtenerRelojZk } = require("../../controller/ZkTeco/zkTeco.controller");
 const router = Router();
+// const edge = require('edge-js')
 
+
+// const myFunc = edge.func(
+//     {
+//         assemblyFile:   path.join(__dirname, 'helpers/libzkfpcsharp.dll'),
+//         typeName: 'libzkfpcsharp.zkfp',
+//         methodName: 'Metodo'
+//     }
+// )
 router.get("/obtener-tipo-cambio", obtenerRelojZk);
 module.exports = router;
