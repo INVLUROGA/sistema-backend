0b293e0f276281923b1637aa6e6acaa84bc6ca8d
personalTrainingp <crosales@personaltraining.com.pe>
2025-06-19 07:29:23 -0500

    flujo caja

--- routes/flujo-caja.router.js
+++ routes/flujo-caja.router.js
@@ -1,16 +1,17 @@
 const { Router } = require("express");
 const {
   getIngresosxMESandAnio,
-  getGastoxGrupo,getCreditoFiscal
+  getGastoxGrupo,
+  getCreditoFiscal,
 } = require("../controller/flujo-caja.controller");
 const router = Router();
 /**
- * [API Documentation]
+ *
  * /api/flujo-caja
  */
 
 router.get("/ingresos", getIngresosxMESandAnio);
 
 router.get("/get-gasto-x-grupo/:id_enterp/:anio", getGastoxGrupo);
-router.get("/credito-fiscal/:id_enterp", getCreditoFiscal)
+router.get("/credito-fiscal/:id_enterp", getCreditoFiscal);
 module.exports = router;
