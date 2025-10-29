42f441c295cf9a64bf2251a177d19013dbf0cf28
Luigui Cruz <luigui2ez4me@gmail.com>
2025-10-29 17:16:36 -0500

    agregado la reserva monkeyfit

--- models/ReservaMonkFit.js
+++ models/ReservaMonkFit.js
@@ -1,48 +1,59 @@
 const { DataTypes } = require("sequelize");
 const { db } = require("../database/sequelizeConnection");
-const ClienteMonkeyFit = require("./ClienteMonkeyFit");
-const { Parametros } = require("./Parametros");
+const { Cliente } = require("../models/Usuarios");
+const { Parametros } = require("../models/Parametros");
 
-const ReservaMonkFit = db.define(
-  "ReservaMonkFit",
-  {
-    id:              { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-    id_cliente_mf:   { type: DataTypes.INTEGER, allowNull: false },
-    id_pgm:          { type: DataTypes.INTEGER, allowNull: false },
-    id_estado_param: { type: DataTypes.INTEGER, allowNull: true },
-    codigo_reserva:  { type: DataTypes.STRING(50), allowNull: true },
-    fecha:           { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
-    monto_total:     { type: DataTypes.DECIMAL(12, 2), allowNull: false, defaultValue: 0 },
-    flag:            { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: true },
+const ReservaMonkFit = db.define("tb_reservaMonkFit", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
   },
-  {
-    tableName: "tb_reservaMonkFit",
-    schema: "dbo",
-    freezeTableName: true,
-    timestamps: false, 
-  }
-);
-
-const recomputeCantReservas = async (id_cliente_mf) => {
-  const total = await ReservaMonkFit.count({ where: { id_cliente_mf, flag: true } });
-  await ClienteMonkeyFit.update({ cant_reservas: total }, { where: { id_cliente_mf } });
-};
-ReservaMonkFit.afterCreate(async (reserva) => recomputeCantReservas(reserva.id_cliente_mf));
-ReservaMonkFit.afterUpdate(async (reserva) => recomputeCantReservas(reserva.id_cliente_mf));
-
-// Relaciones
-ReservaMonkFit.belongsTo(ClienteMonkeyFit, {
-  foreignKey: "id_cliente_mf",
-  targetKey: "id_cliente_mf",
-  as: "cliente",
+  id_cli: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  id_pgm: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  id_estado_param: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  codigo_reserva: {
+    type: DataTypes.STRING(50),
+    allowNull: true,
+  },
+  fecha: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: db.literal("GETDATE()"),
+  },
+  monto_total: {
+    type: DataTypes.DECIMAL(12, 2),
+    allowNull: false,
+    defaultValue: 0.0,
+  },
+  flag: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: true,
+  },
+}, {
+  tableName: "tb_reservaMonkFit",
+  timestamps: false,
 });
 
-if (Parametros) {
-  ReservaMonkFit.belongsTo(Parametros, {
-    foreignKey: "id_estado_param",
-    targetKey: "id_param",
-    as: "estado",
-  });
-}
+
+ReservaMonkFit.belongsTo(Cliente, {
+  as: "cliente",
+  foreignKey: "id_cli",
+});
+
+ReservaMonkFit.belongsTo(Parametros, {
+  as: "estado",
+  foreignKey: "id_estado_param",
+});
 
 module.exports = ReservaMonkFit;
