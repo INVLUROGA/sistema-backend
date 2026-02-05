e8c9d176206fc20024448ef9257a4dcb61950529
personalTrainingp <crosales@personaltraining.com.pe>
2026-02-05 16:09:10 -0500

    desactivar c.2

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const {
   recordatorioReservaCita24hAntes,
   recordatorioReservaCita2hAntes,
   obtenerCumpleaniosDeEmpleados,
-  reactivarAlertasMensuales
+  // reactivarAlertasMensuales
 } = require("./middlewares/eventosCron.js");
 const { enviarBotonesWsp } = require("./config/whatssap-web.js");
 const {
@@ -21,9 +21,9 @@ const {
 // Programa una tarea para las 9 AM todos los días
 // const cum = obtenerDataSeguimientos();
 // console.log(cum);
-cron.schedule("0 3 1 * *", async () => {
-  await reactivarAlertasMensuales();
-});
+// cron.schedule("0 3 1 * *", async () => {
+//   await reactivarAlertasMensuales();
+// });
 
 cron.schedule("0 15 * * *", () => {
   // insertaDatosTEST();
